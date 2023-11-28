bdb3d8c983989d0e9d0fa759e0ae92251a1acf62
Felipe Coelho <felipecoelho.ness@gmail.com>
2023-11-28 09:40:57 -0300

    add: new project img and route

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -4,40 +4,41 @@ export const workSlider = {
     {
       images: [
         {
-          title: 'title',
-          path: '/thumb1.jpg',
+          title: "dashboard",
+          path: "/dashboard.svg",
+          href: "https://dashboard-ness.vercel.app/",
         },
         {
-          title: 'title',
-          path: '/thumb2.jpg',
+          title: "title",
+          path: "/thumb2.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb3.jpg',
+          title: "title",
+          path: "/thumb3.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb4.jpg',
+          title: "title",
+          path: "/thumb4.jpg",
         },
       ],
     },
     {
       images: [
         {
-          title: 'title',
-          path: '/thumb4.jpg',
+          title: "title",
+          path: "/thumb4.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb1.jpg',
+          title: "title",
+          path: "/thumb1.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb2.jpg',
+          title: "title",
+          path: "/thumb2.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb3.jpg',
+          title: "title",
+          path: "/thumb3.jpg",
         },
       ],
     },
@@ -45,65 +46,83 @@ export const workSlider = {
 };
 
 // import swiper react components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 // import required modules
-import { Pagination } from 'swiper';
+import { Pagination } from "swiper";
 
 // icons
-import { BsArrowRight } from 'react-icons/bs';
+import { BsArrowRight } from "react-icons/bs";
 // next image
-import Image from 'next/image';
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 const WorkSlider = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("https://dashboard-ness.vercel.app/");
+  };
+
   return (
     <Swiper
       spaceBetween={10}
       pagination={{
         clickable: true,
       }}
-      modules={[ Pagination ]}
-      className='h-[280px] sm:h-[480px]'
-    >
+      modules={[Pagination]}
+      className="h-[280px] sm:h-[480px]">
       {workSlider.slides.map((slide, index) => {
-          return (
-            <SwiperSlide key={index}>
-              <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
-                  {slide.images.map((image, index) => {
-                  return (
-                    <div
-                      className='relative rounded-lg overflow-hidden flex items-center justify-center group'
-                      key={index}
-                    >
-                    <div className='flex items-center justify-center relative overflow-hidden group'>
+        return (
+          <SwiperSlide key={index}>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+              {slide.images.map((image, index) => {
+                return (
+                  <div
+                    className="relative rounded-lg overflow-hidden flex items-center justify-center group"
+                    key={index}>
+                    <div className="flex items-center justify-center relative overflow-hidden group">
                       {/* image */}
-                      <Image src={image.path} width={500} height={300} alt='' />
+                      <Image
+                        src={image.path}
+                        width={500}
+                        height={300}
+                        alt=""
+                        onClick={handleClick}
+                      />
                       {/* overlay gradient */}
-                      <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
+                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
                       {/* title  */}
-                      <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
-                          <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
-                            {/* title part 1 */}
-                            <div className='delay-100'>LIVE</div>
-                            {/* title part 2 */}
-                            <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>PROJECT</div>
-                            {/* icons */}
-                            <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'> <BsArrowRight/> </div>
+                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
+                        <div
+                          className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]"
+                          onClick={handleClick}>
+                          {/* title part 1 */}
+                          <div className="delay-100">LIVE</div>
+                          {/* title part 2 */}
+                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                            PROJECT
                           </div>
+                          {/* icons */}
+                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
+                            {" "}
+                            <BsArrowRight />{" "}
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
-                  );
-                })}
-              </div>
-            </SwiperSlide>
-          );
-        })}
+                );
+              })}
+            </div>
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
 };
